112c9f61611f329dd057f5e2045bc34917fa10eb
Viorel Lazari <vlazari@gmail.com>
2021-11-04 13:56:00 +0100

    File update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let nav = document.querySelector('.navigation');
+let menue = document.querySelector('.fa-bars');
 const burger = document.querySelector('.mobile-menue');
 burger.addEventListener('click', function()
 {
   nav.classList.toggle('navigation-active');
+  menue.classList.toggle('fa-times');
 });
