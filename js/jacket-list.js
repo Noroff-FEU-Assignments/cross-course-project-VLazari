ec7b154b16fc51710cd2648db210db03dee12337
Viorel Lazari <vlazari@gmail.com>
2021-11-07 19:20:04 +0100

    update images format

---
+++ js/jacket-list.js
@@ -0,0 +1,58 @@
+export const jacketList = [
+  {
+    id: "1",
+    name: "Jacket 1",
+    jacketImg: "../images/products/jacket1.webp",
+    gender: "m",
+    price: 2600,
+  },
+  {
+    id: "2",
+    name: "Jacket 2",
+    jacketImg: "../images/products/jacket2.webp",
+    gender: "f",
+    price: 1800,
+  },
+  {
+    id: "3",
+    name: "Jacket 3",
+    jacketImg: "../images/products/jacket3.webp",
+    gender: "f",
+    price: 2900,
+  },
+  {
+    id: "4",
+    name: "Jacket 4",
+    jacketImg: "../images/products/jacket4.webp",
+    gender: "m",
+    price: 1750,
+  },
+  {
+    id: "5",
+    name: "Jacket 5",
+    jacketImg: "../images/products/jacket5.webp",
+    gender: "f",
+    price: 2200,
+  },
+  {
+    id: "6",
+    name: "Jacket 6",
+    jacketImg: "../images/products/jacket6.webp",
+    gender: "m",
+    price: 3800,
+  },
+  {
+    id: "7",
+    name: "Jacket 7",
+    jacketImg: "../images/products/jacket7.webp",
+    gender: "f",
+    price: 1400,
+  },
+  {
+    id: "8",
+    name: "Jacket 8",
+    jacketImg: "../images/products/jacket8.webp",
+    gender: "m",
+    price: 2180,
+  }
+];
